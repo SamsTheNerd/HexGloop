9a18b02b88d55584e88339281b418c6074f3e4dd
SamsTheNerd <sam@samsthenerd.com>
2023-08-03 17:25:16 -0400

    0.0.2 mostly there, trying on server

--- changelog.md
+++ changelog.md
@@ -2,6 +2,10 @@
 
 ## 0.0.2
 
+### Added
+
+- Click to copy list
+
 ### Fixed
 
 - Not launching on server
@@ -9,6 +13,7 @@
 - Spellbook not being label-able
 - Trying to label unlabel-able item failing quietly (now throws a mishap)
 - Casting Potion crash with hexxycraft hexcasting build
+- Rival's gambit showing as lover's gambit in book
 
 ### Changes
 
--- common/src/main/java/com/samsthenerd/hexgloop/mixins/textpatterns/MixinChangeListDisplay.java
+++ common/src/main/java/com/samsthenerd/hexgloop/mixins/textpatterns/MixinChangeListDisplay.java
@@ -2,17 +2,25 @@ package com.samsthenerd.hexgloop.mixins.textpatterns;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.samsthenerd.hexgloop.HexGloop;
 
+import at.petrak.hexcasting.api.spell.iota.PatternIota;
+import at.petrak.hexcasting.api.spell.math.HexPattern;
 import at.petrak.hexcasting.api.utils.HexUtils;
 import at.petrak.hexcasting.common.lib.hex.HexIotaTypes;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
+import net.minecraft.text.ClickEvent;
 import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
 
 @Mixin(targets = "at.petrak.hexcasting.api.spell.iota.ListIota$1")
 public class MixinChangeListDisplay {
@@ -31,4 +39,38 @@ public class MixinChangeListDisplay {
         }
         return text;
     }
+
+    // add copy thingy for list
+    @Inject(method = "display(Lnet/minecraft/nbt/NbtElement;)Lnet/minecraft/text/Text;", at = @At("RETURN"), cancellable = true)
+    public void copyfullText(NbtElement tag, CallbackInfoReturnable<Text> cir){
+        // basically just yoink inside of list creating method
+        String copyText = "[";
+        NbtList list = HexUtils.downcast(tag, NbtList.TYPE);
+        for (int i = 0; i < list.size(); i++) {
+            NbtElement sub = list.get(i);
+            NbtCompound csub = HexUtils.downcast(sub, NbtCompound.TYPE);
+
+            if(HexIotaTypes.getTypeFromTag(csub) == PatternIota.TYPE){
+                // handle pattern:
+                NbtCompound tagData = csub.getCompound(HexIotaTypes.KEY_DATA);
+                if(tagData == null || tagData.isEmpty()) continue;
+                HexPattern pattern = HexPattern.fromNBT(tagData);
+                HexGloop.logPrint(csub.toString() + " => " + pattern.toString() + "\n");
+                copyText += ("<" + pattern.getStartDir().toString().replace("_", "").toLowerCase() + "," + pattern.anglesSignature() + ">");
+            } else {
+                copyText += HexIotaTypes.getDisplay(csub).getString();
+            }
+
+
+            if (i < list.size() - 1) {
+                copyText += (", ");
+            }
+        }
+        copyText += "]";
+        MutableText listText = cir.getReturnValue().copy();
+        Style clickEventStyle = Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, copyText));
+        listText.setStyle(clickEventStyle.withParent(listText.getStyle()));
+        cir.setReturnValue(listText);
+    }
+    
 }
